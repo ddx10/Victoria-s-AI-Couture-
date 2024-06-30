d62da54505042c529d27774e6d878c79ace9b231
ddx10 <37805153+ddx10@users.noreply.github.com>
2024-06-30 15:13:23 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const faqItems = document.querySelectorAll('.faq-item');
+    const galleryItems = document.querySelectorAll('.gallery-item');
 
-    faqItems.forEach(item => {
-        const question = item.querySelector('.faq-question');
-        question.addEventListener('click', () => {
-            const answer = item.querySelector('.faq-answer');
-            const isVisible = answer.style.display === 'block';
+    galleryItems.forEach(item => {
+        const img = item.querySelector('img');
+        const overlayMessage = item.querySelector('.overlay-message');
+        
+        img.addEventListener('mouseenter', () => {
+            overlayMessage.style.opacity = '1';
+        });
 
-            // Ocultar todas las respuestas
-            document.querySelectorAll('.faq-answer').forEach(ans => ans.style.display = 'none');
-
-            // Alternar la visibilidad de la respuesta actual
-            answer.style.display = isVisible ? 'none' : 'block';
+        img.addEventListener('mouseleave', () => {
+            overlayMessage.style.opacity = '0';
         });
     });
 });
+
