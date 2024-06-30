407152d027db941fc82534e5adba35d7d821da1a
ddx10 <37805153+ddx10@users.noreply.github.com>
2024-06-30 16:05:12 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function showRandomPopup() {
-        const allImages = document.querySelectorAll('.images');
+        const allImages = document.querySelectorAll('.gallery-item img');
         const randomImage = allImages[Math.floor(Math.random() * allImages.length)];
         const randomMessage = messages[Math.floor(Math.random() * messages.length)];
         createPopup(randomMessage, randomImage.src);
